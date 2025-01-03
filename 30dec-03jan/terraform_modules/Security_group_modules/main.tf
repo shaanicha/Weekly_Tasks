ad65bb30308a9acf6adf5a89e3c82c89be9282c1
Shraddha Chaudhari <127373378+shaanicha@users.noreply.github.com>
2025-01-03 17:40:14 +0530

    Create main.tf

---
+++ 30dec-03jan/terraform_modules/Security_group_modules/main.tf
@@ -0,0 +1,38 @@
+# modules/security_group/main.tf
+resource "aws_security_group" "ec2" {  # Note: name matches the output reference
+  name_prefix = "ec2-sg-"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "alb" {  # Note: name matches the output reference
+  name_prefix = "alb-sg-"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
