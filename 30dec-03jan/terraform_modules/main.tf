31e0875e1feb061742c20c565d1f0d69a0843ec6
Shraddha Chaudhari <127373378+shaanicha@users.noreply.github.com>
2025-01-03 15:24:24 +0530

    Update main.tf

--- 30dec-03jan/terraform_modules/main.tf
+++ 30dec-03jan/terraform_modules/main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source          = "./modules/ec2"
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  ami_id          = var.ami_id
+  key_name        = var.key_name
+}
+
+module "alb" {
+  source           = "./modules/alb"
+  vpc_id           = module.vpc.vpc_id
+  public_subnets   = module.vpc.public_subnets
+  target_group_arn = module.ec2.target_group_arn
+}
 
